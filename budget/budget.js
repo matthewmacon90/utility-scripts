3ed31c2d70e01bc0fa43b39af0d622f58867fa0d
Matthew Macon <matthewmacon90@gmail.com>
2025-02-13 19:19:07 -0500

    created script to determine monthly income

---
+++ budget/budget.js
@@ -0,0 +1,40 @@
+const budget = (totalMonthyBills=2000, grossIncomeYear=30000, federalTax=22, stateIncomeTax=3, countyTax=2, benefits=300, retirement=5) => {
+    const totalBudgetObject = {
+        totalMonthyBills,
+        grossIncomeYear,
+        federalTax,
+        stateIncomeTax,
+        countyTax,
+        benefits,
+        retirement,
+        totalIncome: 0,
+    };
+    const monthlyIncome = grossIncomeYear / 12;
+    console.log(`Your monthly income is $${monthlyIncome.toFixed(2)}`);
+
+    const incomeAfterRetirement = monthlyIncome - (monthlyIncome * retirement / 100);
+    console.log(`Your monthly income after retirement of ${retirement}% is $${incomeAfterRetirement.toFixed(2)}`);
+    console.log(`Your monthly income before taxes is $${incomeAfterRetirement.toFixed(2)}`);
+
+    const taxedIncome = incomeAfterRetirement - (incomeAfterRetirement * (federalTax + stateIncomeTax + countyTax) / 100);
+    console.log(`Federal Tax: ${federalTax}% , State Tax: ${stateIncomeTax}%, County Tax: ${countyTax}%, total tax: ${federalTax + stateIncomeTax + countyTax}%`);
+    console.log(`Total monthly taxed amount: $${(incomeAfterRetirement * (federalTax + stateIncomeTax + countyTax) / 100).toFixed(2)}`);
+    console.log(`Your monthly income after taxes is $${taxedIncome.toFixed(2)}`);
+
+    const incomeAfterBenefits = taxedIncome - benefits;
+    console.log(`Your monthly income after benefits is $${incomeAfterBenefits.toFixed(2)}`);
+
+    const payEveryTwoWeeks = incomeAfterBenefits / 2;
+    console.log(`You should be paid $${payEveryTwoWeeks.toFixed(2)} every two weeks`);
+    
+    const incomeAfterBills = incomeAfterBenefits - totalMonthyBills;
+    console.log(`Your monthly income after bills is $${incomeAfterBills.toFixed(2)}`);
+    totalBudgetObject.totalIncome = incomeAfterBills;
+
+    console.log(`Your total income per month is: $${totalBudgetObject.totalIncome.toFixed(2)}`);
+
+    return totalBudgetObject;
+};
+
+budget(2897.71, 70000, 22, 2.75, 2.5, 180, 5);
+// budget();
